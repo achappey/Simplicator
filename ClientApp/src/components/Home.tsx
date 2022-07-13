06aa8b76b002df607c9d960a7f27da680e500907
Arthur Bleij <github@achappey.nl>
2022-07-13 09:26:47 +0200

    Added some documentation

--- ClientApp/src/components/Home.tsx
+++ ClientApp/src/components/Home.tsx
@@ -10,8 +10,17 @@ export const Home: React.FunctionComponent = () => {
       <p>Add as query string or in the request header:</p>
       <code>x-api-key: base64Encode([SimplicateApiKey]:[SimplicateApiSecret]@[SimplicateEnvironment])</code>
       <p>
-      Calculate your API Key <Link href={"/access"}>here</Link>.
+        Calculate your API Key <Link href={"/access"}>here</Link>.
       </p>
+      <h4>Power BI</h4>
+      <p>For example, get all your Simplicate projects:</p>
+      <code>
+        OData.Feed("https://simplicator.net/odata/projects", null, [ApiKeyName="x-api-key", Implementation = "2.0"])
+      </code>
+      <p>Authenticate with Web API and enter your API Key</p>
+
+      <h4>Power Apps</h4>
+      <p>Create a custom connector from <Link href={"/swagger/v2/swagger.json"}>this</Link> file. More info <Link href={"https://docs.microsoft.com/en-us/connectors/custom-connectors/define-openapi-definition"} target={"_blank"}>here</Link>.</p>
       <h4>Documentation</h4>
       <ul>
         <li>Swagger: <Link href={"/swagger/index.html"} target={"_blank"}>HTML</Link> | <Link href={"/swagger/v2/swagger.json"} target={"_blank"}>JSON</Link> | <Link href={"/swagger/v2/swagger.yaml"} target={"_blank"}>YAML</Link></li>
