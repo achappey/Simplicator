db92c6858266f10b2f00fc0c5cd59b75c5b50be7
Arthur Bleij <github@achappey.nl>
2023-07-22 20:40:50 +0200

    Refactor selectQuery in GetTimeEntries method

--- Simplicate.NET/Extensions/HttpExtensions.cs
+++ Simplicate.NET/Extensions/HttpExtensions.cs
@@ -181,13 +181,13 @@ public static class HttpExtensions
     {
         string requestPath = Endpoints.Hours.TimeEntry;
         string selectQuery = @"select=id,employee.,projectservice.,
-                           project.,invoice_status,start_date,created_at,
-                           end_date,hours,billable,status,updated_at,
+                           project.,invoice_status,start_date,
+                           end_date,hours,status,
                            billable,tariff,note";
-
+//created_at,updated_at
         Uri requestUri = environment.BuildRequestUri(requestPath, null, selectQuery);
 
-        return await client.PagedRequest<Hours>(requestUri, key, secret);
+        return await client.PagedRequest<Hours>(requestUri, key, secret, 500);
     }
 
     /// <summary>
